6248a09da6e3a5b608db3bb16a0e7cce728508b1
ll-hyper <priyankabillo49@gmail.com>
2025-03-14 09:32:52 +0530

    Update ping.py

--- AarohiX/modules/ping.py
+++ AarohiX/modules/ping.py
@@ -67,7 +67,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нey вαву!!\n{AarohiX.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴅɪʟ❣️](https://t.me/{hyper_trader_o_p}) ||</b>",
+        text=f"нey вαву!!\n{AarohiX.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴅɪʟ❣️](https://t.me/hyper_trader_o_p) ||</b>",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
