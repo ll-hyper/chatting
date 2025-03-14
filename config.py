1497d694c9632637256725bf50621bcd61b73cca
ll-hyper <priyankabillo49@gmail.com>
2025-03-14 09:36:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "14050586"
-API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
+API_ID = "21787806"
+API_HASH = "e21b0b6d085d8dc63b9455c98eb2ef4a"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "5465943450"
-SUPPORT_GRP = "LOVE_FEELINGS_WILL1"
-UPDATE_CHNL = "alonegroup121"
-OWNER_USERNAME = "dil_sagar_121"
+SUPPORT_GRP = "FRIENSHIP_CLUB_GROUP"
+UPDATE_CHNL = "FEELING_SMILEY"
+OWNER_USERNAME = "Aashi_chatting_bot"
