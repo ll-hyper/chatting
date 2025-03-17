d6403e91c693536a67a887ad9697598276fba2f9
ll-hyper <priyankabillo49@gmail.com>
2025-03-17 13:31:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "21787806"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "e21b0b6d085d8dc63b9455c98eb2ef4a"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = getenv("OWNER_ID", "1808943146")
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = getenv("OWNER_ID", "8176091341")
+SUPPORT_GRP = "FRIENSHIP_CLUB_GROUP"
+UPDATE_CHNL = "FEELING_SMILEY"
+OWNER_USERNAME = "hyper_trader_o_p"
 
